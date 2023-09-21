c68af026eda6f09a28e0dc73130c567821683131
cjhieras <145633720+cjhieras@users.noreply.github.com>
2023-09-21 12:35:53 +0800

    Add files via upload

---
+++ COR 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	int term;
+	int sy;
+	int st;
+	
+	char [50] name;
+	char [50] college;
+	char [50 ]program;
+	
+cout <<"Term: ";
+cin >> term;
+cout <<"School Year: ";
+cin >> sy;
+
+
+	cout <<"Enrollement Status: Enrolled" <<endl;
+	cout << "Term: 1 School Year 2023-2024" <<endl;
+	
+	cout << "Student Number: 202010784				College: Information Technology	Program: WMA" <<endl;
+	cout << "Name: Hieras, Jenrev Colleen M.				Year Level: 1	SY: 2023-2024" <<endl;
+	
+	cout <<endl;
+	
+	cout << "COURSES" << "		TITE		" << "	SECTION	" << "	UNITS	" << "	DAYS	" << "	TIME	" << "	ROOM	" <<endl;
+	
+	
+	
+	return 0;	
+}
